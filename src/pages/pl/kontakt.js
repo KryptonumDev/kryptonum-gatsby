fca2149fb1008edf955d1c00911a7fae488de24b
Bogumił <hello@milewski.me>
2023-06-29 14:36:31 +0200

    added GraphQL queries to Contact page

--- src/pages/pl/kontakt.js
+++ src/pages/pl/kontakt.js
@@ -22,6 +22,7 @@ const ContactPage = ({
 export const query = graphql`
   query {
     page: sanityContact {
+      # Hero
       hero_Heading
       hero_Subheading
       hero_Contact {
@@ -38,6 +39,28 @@ export const query = graphql`
           }
         }
       }
+      # Contact
+      contact_Email
+      contact_Tel
+      contact_Address
+      contact_Img {
+        asset {
+          altText
+          gatsbyImageData(placeholder: BLURRED)
+        }
+      }
+      # Pricing
+      pricing_Paragraph
+      pricing_Paragraph2
+      pricing_Cta {
+        theme
+        text
+        href
+      }
+      helpDesk_Heading
+      helpDesk_Subheading
+      helpDesk_Paragraph
+      helpDesk_FormTitle
       seo {
         title
         description
