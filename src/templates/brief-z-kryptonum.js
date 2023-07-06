076f2b23ef18ee50eab29ecdb6a1d2b572c55061
Bogumił <hello@milewski.me>
2023-07-06 15:11:58 +0200

    added SEO redirects

--- src/templates/brief-z-kryptonum.js
+++ src/templates/brief-z-kryptonum.js
@@ -7,6 +7,77 @@ import Summary from "../components/sections/ExtendedContactSummary";
 import { AnimatePresence, motion } from "framer-motion";
 import Loader from "../components/atoms/Loader";
 
+const csvData = `https://kryptonum.eu/;https://kryptonum.eu/pl
+https://kryptonum.eu/projekty/dwa-slowa/;https://kryptonum.eu/pl/portfolio/dwa-slowa
+https://kryptonum.eu/projekty/brygida-helbig/;https://kryptonum.eu/pl/portfolio/brygida-helbig
+https://kryptonum.eu/projekty/;https://kryptonum.eu/pl/portfolio
+https://kryptonum.eu/oferta/;https://kryptonum.eu/pl
+https://kryptonum.eu/projekty/9m13/;https://kryptonum.eu/pl/portfolio/9m13
+https://kryptonum.eu/kontakt/;https://kryptonum.eu/pl/kontakt/
+https://kryptonum.eu/blog/jak-rozmawiac-z-tworcami-stron-internetowych/;https://kryptonum.eu/pl/blog/jak-rozmawiac-z-tworcami-stron-internetowych
+https://kryptonum.eu/projekty/centrum-korekty/;https://kryptonum.eu/pl/portfolio/centrum-korekty/
+https://kryptonum.eu/faq/;https://kryptonum.eu/pl/faq/
+https://kryptonum.eu/blog/strony-internetowe-na-lata/;https://kryptonum.eu/pl/blog/strony-internetowe-na-lata
+https://kryptonum.eu/blog/;https://kryptonum.eu/pl/blog
+https://kryptonum.eu/projekty/wloski-od-zera/;https://kryptonum.eu/pl/portfolio/wloski-od-zera
+https://kryptonum.eu/polityka-prywatnosci/;https://kryptonum.eu/pl/polityka-prywatnosci/
+https://kryptonum.eu/o-nas/;https://kryptonum.eu/pl/o-nas
+https://kryptonum.eu/blog/strona-internetowa-czy-media-spolecznosciowe/;https://kryptonum.eu/pl/blog/strona-internetowa-czy-media-spolecznosciowe
+https://kryptonum.eu/projekty/altwork/;https://kryptonum.eu/pl/portfolio/altwork
+https://kryptonum.eu/projekty/cargem/;https://kryptonum.eu/pl/portfolio/cargem
+https://kryptonum.eu/projekty/justyna-holosyniuk/;https://kryptonum.eu/pl/portfolio/justyna-holosyniuk
+https://kryptonum.eu/projekty/xminer/;https://kryptonum.eu/pl/portfolio/xminer
+https://kryptonum.eu/blog/jak-zdobyc-pierwszego-klienta/;https://kryptonum.eu/pl/blog/jak-zdobyc-pierwszego-klienta
+https://kryptonum.eu/blog/3-sposoby-na-rozpoznawalnosc-twojej-marki/;https://kryptonum.eu/pl/blog/3-sposoby-na-rozpoznawalnosc-twojej-marki
+https://kryptonum.eu/blog/jak-zdobyc-pierwszego-klienta/mailtokuba@kryptonum.eu;https://kryptonum.eu/pl
+https://kryptonum.eu/page-data/blog/jak-zdobyc-pierwszego-klienta/mailtokuba@kryptonum.eu/page-data.json;https://kryptonum.eu/pl
+https://kryptonum.eu/projekty/dwa-slowa;https://kryptonum.eu/pl/portfolio/dwa-slowa
+https://kryptonum.eu/projekty/brygida-helbig;https://kryptonum.eu/pl/portfolio/brygida-helbig
+https://kryptonum.eu/projekty;https://kryptonum.eu/pl/portfolio
+https://kryptonum.eu/oferta;https://kryptonum.eu/pl
+https://kryptonum.eu/projekty/9m13;https://kryptonum.eu/pl/portfolio/9m13
+https://kryptonum.eu/portfolio;https://kryptonum.eu/pl/portfolio
+https://kryptonum.eu/blog/jak-rozmawiac-z-tworcami-stron-internetowych;https://kryptonum.eu/pl/blog/jak-rozmawiac-z-tworcami-stron-internetowych
+https://kryptonum.eu/projekty/centrum-korekty;https://kryptonum.eu/pl/portfolio/centrum-korekty
+https://www.kryptonum.eu/projekty/centrum-korekty;https://kryptonum.eu/pl/portfolio/centrum-korekty
+https://kryptonum.eu/faq;https://kryptonum.eu/pl/faq
+https://kryptonum.eu/blog/strony-internetowe-na-lata;https://kryptonum.eu/pl/blog/strony-internetowe-na-lata
+https://kryptonum.eu/blog;https://kryptonum.eu/pl/blog
+https://kryptonum.eu/projekty/wloski-od-zera;https://kryptonum.eu/pl/portfolio/wloski-od-zera
+https://kryptonum.eu/polityka-prywatnosci;https://kryptonum.eu/pl/polityka-prywatnosci
+https://kryptonum.eu/o-nas;https://kryptonum.eu/pl/o-nas
+https://kryptonum.eu/blog/strona-internetowa-czy-media-spolecznosciowe;https://kryptonum.eu/pl/blog/strona-internetowa-czy-media-spolecznosciowe
+https://kryptonum.eu/projekty/altwork;https://kryptonum.eu/pl/portfolio/altwork
+https://kryptonum.eu/projekty/cargem;https://kryptonum.eu/pl/portfolio/cargem
+https://kryptonum.eu/projekty/justyna-holosyniuk;https://kryptonum.eu/pl/portfolio/justyna-holosyniuk
+https://kryptonum.eu/projekty/xminer;https://kryptonum.eu/pl/portfolio/xminer
+https://kryptonum.eu/blog/jak-zdobyc-pierwszego-klienta;https://kryptonum.eu/pl/blog/jak-zdobyc-pierwszego-klienta
+https://kryptonum.eu/blog/3-sposoby-na-rozpoznawalnosc-twojej-marki;https://kryptonum.eu/pl/blog/3-sposoby-na-rozpoznawalnosc-twojej-marki
+https://kryptonum.eu/sitemap-index.xml;https://kryptonum.eu/pl/sitemap-index.xml
+https://kryptonum.eu/sitemap-0.xml;https://kryptonum.eu/pl/sitemap-0.xml
+`
+function csvToJson(csvString) {
+  const lines = csvString.trim().split('\n');
+  const headers = ['fromPath', 'toPath'];
+  const json = [];
+
+  for (let i = 1; i < lines.length; i++) {
+    const fields = lines[i].split(';').map(field => field.trim());
+    const redirect = {};
+
+    for (let j = 0; j < headers.length; j++) {
+      redirect[headers[j]] = fields[j].replace('https://kryptonum.eu', '');
+    }
+    redirect['isPermanent'] = true;
+
+    json.push(redirect);
+  }
+
+  return json;
+}
+const jsonData = csvToJson(csvData);
+console.log(JSON.stringify(jsonData, null, 2));
+
 const BriefPage = ({ data }) => {
 
   const [step, setStep] = useState(0)
